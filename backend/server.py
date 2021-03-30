fbc90eb6105e3500fdd87c76a44e5f4b00ceac34
lionel <lioneldg92@gmail.com>
2021-03-30 13:41:59 +0200

    Installing packages to run mongodb in python : pymongo, flask_cors, dotenv, dnspython and setting up database

--- backend/server.py
+++ backend/server.py
@@ -2,14 +2,24 @@ from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
 from pymongo import MongoClient
+import os
+import pymongo
+from pymongo import MongoClient
+
 
 app = Flask(__name__)
 
 # Package allowing us to use .env 
 load_dotenv()
 
+client = MongoClient(os.getenv("DATABASE_URL"))
+db = client['businessDB']
+collection = db['test-collection']
+
 @app.route('/')
 def hello_world():
+    all = collection.find()
+    print(all)
     return 'Hello, World!'
 
 if __name__ == "__main__":        
