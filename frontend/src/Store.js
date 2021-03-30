4129bcca0c20ce940ad9a7515856cc7eb7995fdf
lionel <lioneldg92@gmail.com>
2021-03-30 14:07:29 +0200

    Adding mechanism to store token in browser memory for auth and session

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,4 +1,9 @@
 import { createStore } from 'redux';
+import {loadState, saveState} from './components/LocalStorage';
+
+// We are using browser memory to store our Authentication and Session Token
+// -> JWT
+const persistedState = loadState();
 
 const initState = {
     titleHome: "Welcome to the App",
@@ -12,9 +17,12 @@ const Store = (state = initState, action) => {
     }
     return state
 };
-const store = createStore(Store);
+const store = createStore(Store, persistedState);
 
 store.subscribe(() => {
     console.log("state updated", store.getState());
+    saveState({
+        access_token: store.getState().access_token,
+    });
 });
 export default store;   
