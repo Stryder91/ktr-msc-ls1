d0592089d1514a05c9005d17de9997e788d92bfc
lionel <lioneldg92@gmail.com>
2021-03-30 14:12:29 +0200

    Add UI Business Card

---
+++ frontend/src/components/BusinessCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Card, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+
+const MyCard = ({user}) => {
+  return (
+    <div className="my-1">
+      <Card className="col-lg-4 m-auto">
+        <CardBody>
+          <CardTitle tag="h5">Business card - {user.name}</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">{user.email}</CardSubtitle>
+          <CardText>Currently working in : {user.company}</CardText>
+          <CardText>Contact : {user.telephone}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default MyCard;
