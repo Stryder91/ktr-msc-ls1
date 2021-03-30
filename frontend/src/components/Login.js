82fac7eeef5268ec2e152ec09fe932ee40c7e494
lionel <lioneldg92@gmail.com>
2021-03-30 14:49:56 +0200

    Set Token once logged

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import axios from 'axios';
+
 import { Col, Row, FormGroup, Input, Button } from 'reactstrap';
 
-const Login = () => {
+const Login = ({set_Token}) => {
 
     const [cred, setCred] = useState({email: '', name:'' , password: '', company: '', telephone:''});
 
     const Login = () => {
-        axios.post('http://localhost:5000/register', cred)
+        axios.post('http://localhost:5000/login', cred)
         .then(res => {
-            console.log(res)
+            if (res.data && 'access_token' in res.data) {
+                set_Token({'token': res.data.access_token, 'user': res.data.username});
+            } else {
+                console.log("No access TOKEN!")
+            }
         })
         .catch(err => {
           console.log("Error login", err)
@@ -73,5 +79,17 @@ const Login = () => {
         </div>
     );
 }
-
-export default Login;
+const getProps = state => {
+    return {
+      tokenFromStore : state.access_token,
+    }
+}
+// to SET to the store as dispatch
+const setProps = dispatch => {
+    return {
+        set_Token: tok => {
+            dispatch({type: "SET_TOKEN", tok});
+        },
+    }
+};  
+export default connect(getProps, setProps)(Login);
